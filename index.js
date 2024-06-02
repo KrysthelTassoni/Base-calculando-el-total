d9cfdf9e8b0aa1f6a4982f569b3fae9730959032
Krysthel Tassoni <krystheltassoni@gmail.com>
2024-06-02 11:52:12 -0400

    Se agrego addEventLintener para suma y resta

---
+++ index.js
@@ -0,0 +1,33 @@
+precio = 400000;
+
+precioSpan = document.querySelector(".precio-inicial");
+precioSpan.innerHTML = precio;
+
+let sumar = document.querySelector("#sumar");
+let restar = document.querySelector("#restar");
+
+restar.disabled = true;
+
+sumar.addEventListener("click", () => {
+  let cantidad = document.querySelector(".cantidad");
+  let valor = cantidad.innerHTML;
+  cantidad.innerHTML = parseInt(valor) + 1;
+  let total = document.querySelector(".valor-total");
+  total.innerHTML = parseInt(cantidad.innerHTML) * precio;
+  restar.disabled = false;
+});
+
+restar.addEventListener("click", () => {
+  let cantidad = document.querySelector(".cantidad");
+  let valor = cantidad.innerHTML;
+  cantidad.innerHTML = parseInt(valor) - 1;
+  let total = document.querySelector(".valor-total");
+  total.innerHTML = parseInt(cantidad.innerHTML) * precio;
+  if (valor == 1) {
+    restar.disabled = true;
+  }
+}); 
+
+
+
+
